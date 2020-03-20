64a31018210f554a074ae5ac6dce799e02b5899d
sercanfirtina <sercanfirtina@gmail.com>
2020-03-20 17:57:45 +0300

    MVC UserInterface Created

---
+++ UI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Entity;
+using Entity.SqlQuery;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            GetEmployees();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        #region Employee Partial
+
+        public ActionResult _partialEmployeeList()
+        {
+            return View(GetEmployees());
+        }
+
+
+        #endregion
+
+
+
+        #region EmployeeCrud
+        public List<Employee> GetEmployees()
+        {
+            var employeeResult = MvcDBHelper.Repository.GetAll<Employee>(QueryWarehouse.Employee.GetAll).ToList();
+            return employeeResult;
+        } 
+        #endregion
+    }
+}
---
+++ UI/Models/MvcDBHelper.cs
@@ -0,0 +1,37 @@
+﻿using Entity.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public sealed class MvcDBHelper
+    {
+        private static volatile AbstractDapperRepo _repositoryInstance;
+
+        private static object _syncRoot = new object();
+
+
+        public MvcDBHelper()
+        {
+
+        }
+
+        public static AbstractDapperRepo Repository
+        {
+            get
+            {
+                if (_repositoryInstance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_repositoryInstance == null)
+                             _repositoryInstance = new DapperRepository("ConnectionName");
+                    }
+                }
+                return _repositoryInstance;
+            }
+        }
+    }
+}
